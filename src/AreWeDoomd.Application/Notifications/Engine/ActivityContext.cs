45ad60d916f1141e5e439ff20408b1696ab00c6b
Doğa Koçak <doga_kocak0623@hotmail.com>
2026-06-04 21:12:43 +0300

    fix: add ActorType field to ActivityContext

--- src/AreWeDoomd.Application/Notifications/Engine/ActivityContext.cs
+++ src/AreWeDoomd.Application/Notifications/Engine/ActivityContext.cs
@@ -3,6 +3,7 @@ namespace AreWeDoomd.Application.Notifications.Engine;
 public sealed record ActivityContext(
     string ActivityType,
     string ActorId,
+    string ActorType,
     string ActorDisplayName,
     string ObjectId,
     string ObjectType,
